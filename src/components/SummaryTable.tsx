69a8197f66527126559f60936893f8335c85a1d9
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2023-01-20 13:18:07 -0300

    feat: added summary table page prototype.

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,3 +1,21 @@
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+
 export function SummaryTable() {
-  return <div></div>;
+  return (
+    <div className="w-full flex">
+      <div className="grid-rows-7 grid-flow-col gap-3">
+        {weekDays.map((weekDay, i) => (
+          <div
+            key={`${weekDay}-${i}`}
+            className="text-zinc-400 font-bold text-xl h-10 w-10 flex items-center justify-center"
+          >
+            {weekDay}
+          </div>
+        ))}
+      </div>
+      <div className="grid-rows-7 grid-flow-row gap-3">
+        <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg "></div>
+      </div>
+    </div>
+  );
 }
