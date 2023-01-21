bcab4ca2eed416f0823a938156f0d9e9f0d99a47
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2023-01-21 19:22:46 -0300

    feat: added Checkbox component to HabitDay.

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,5 +1,7 @@
 import * as Popover from "@radix-ui/react-popover";
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { ProgressBar } from "./ProgressBar";
+import { Check } from "phosphor-react";
 import clsx from "clsx";
 
 interface HabitProps {
@@ -31,6 +33,18 @@ export function HabitDay(props: HabitProps) {
           <span className="font-semibold text-zinc-400"></span>
           <span className="mt-1 font-extrabold leading-tight text-3xl"></span>
           <ProgressBar progress={percentage} />
+          <div className="mt-6 flex flex-col gap-3">
+            <Checkbox.Root className="flex items-center gap-3 group">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+                Á
+              </span>
+            </Checkbox.Root>
+          </div>
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
       </Popover.Portal>
